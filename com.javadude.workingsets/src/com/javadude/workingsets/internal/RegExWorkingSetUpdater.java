70c769241cea2680134b32d4bff7a1f318fb8e77
scott <scott@javadude.com>
2009-02-23 18:36:36 +0000

    refactored working set lists into common updater base class

--- com.javadude.workingsets/src/com/javadude/workingsets/internal/RegExWorkingSetUpdater.java
+++ com.javadude.workingsets/src/com/javadude/workingsets/internal/RegExWorkingSetUpdater.java
@@ -7,9 +7,6 @@
  *******************************************************************************/
 package com.javadude.workingsets.internal;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.regex.Pattern;
 
 import org.eclipse.core.resources.IProject;
@@ -23,17 +20,11 @@ import com.javadude.workingsets.DynamicWorkingSetUpdater;
  * @author Scott Stanchfield
  */
 public class RegExWorkingSetUpdater extends DynamicWorkingSetUpdater {
-	private static final Map<String, IWorkingSet> workingSets_ = Collections.synchronizedMap(new HashMap<String, IWorkingSet>());
 	@Override protected String getId(IWorkingSet workingSet) {
 		String id = workingSet.getName();
 		return id.substring("RegEx: ".length());
 	}
 	@Override protected boolean shouldInclude(IProject project, String workingSetId) {
 		return (Pattern.matches(workingSetId, project.getName()));
-	}
-	@Override
-	protected Map<String, IWorkingSet> getAllWorkingSetsOfThisType() {
-		// TODO Auto-generated method stub
-		return workingSets_;
 	}
 }
