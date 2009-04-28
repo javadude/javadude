ac077dc214050a214288ca0bb9cdbbecded75030
scott <scott@javadude.com>
2009-04-28 00:04:13 +0000

    add copyright stuff

--- com.javadude.derived.warning.cleaner/src/com/javadude/derived/warning/cleaner/builder/DerivedWarningCleanerBuilder.java
+++ com.javadude.derived.warning.cleaner/src/com/javadude/derived/warning/cleaner/builder/DerivedWarningCleanerBuilder.java
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Scott Stanchfield
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
 package com.javadude.derived.warning.cleaner.builder;
 
 import java.util.Map;
