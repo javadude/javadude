cb073d40828b1783a74812d43ae7671658f2e2b3
scott@javadude.com <scott@javadude.com@88cfd0be-b251-0410-a78a-4b8c7298577f>
2009-07-22 02:20:35 +0000

    Share project "com.javadude.cleanup" into "https://javadude.googlecode.com/svn"

---
+++ trunk/com.javadude.cleanup/src/com/javadude/cleanup/JavaDudeCleanUpConfigurationUI.java
@@ -0,0 +1,76 @@
+package com.javadude.cleanup;
+
+import org.eclipse.jdt.ui.cleanup.CleanUpOptions;
+import org.eclipse.jdt.ui.cleanup.ICleanUpConfigurationUI;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+
+public class JavaDudeCleanUpConfigurationUI implements
+		ICleanUpConfigurationUI {
+
+	private CleanUpOptions options;
+
+	public JavaDudeCleanUpConfigurationUI() {
+	}
+
+	@Override
+	public Composite createContents(Composite parent) {
+		Composite composite = new Composite(parent, SWT.NONE);
+		GridLayout layout = new GridLayout();
+		composite.setLayout(layout);
+		layout.numColumns = 3;
+		final Button convertButton = new Button(composite, SWT.CHECK);
+		convertButton.setLayoutData(new GridData(SWT.FILL, SWT.NONE, true, false, 3, 1));
+		convertButton.setText("Convert Tabs to Spaces");
+		convertButton.setSelection(options.isEnabled(JavaDudeCleanUpOptionsInitializer.CONVERT_TABS_TO_SPACES));
+		convertButton.addSelectionListener(new SelectionListener() {
+			@Override public void widgetSelected(SelectionEvent e) {
+				options.setOption(JavaDudeCleanUpOptionsInitializer.CONVERT_TABS_TO_SPACES, convertButton.getSelection() ? CleanUpOptions.TRUE : CleanUpOptions.FALSE);
+			}
+			@Override public void widgetDefaultSelected(SelectionEvent e) {
+				options.setOption(JavaDudeCleanUpOptionsInitializer.CONVERT_TABS_TO_SPACES, convertButton.getSelection() ? CleanUpOptions.TRUE : CleanUpOptions.FALSE);
+			}});
+		Label label = new Label(composite, SWT.NONE);
+		label.setText("      ");
+		label = new Label(composite, SWT.NONE);
+		label.setText("Tab Size: ");
+		final Text text = new Text(composite, SWT.SINGLE | SWT.BORDER);
+		text.setLayoutData(new GridData(SWT.FILL, SWT.NONE, true, false));
+		text.setText(options.getValue(JavaDudeCleanUpOptionsInitializer.TAB_SIZE));
+		text.addModifyListener(new ModifyListener() {
+			@Override public void modifyText(ModifyEvent e) {
+				options.setOption(JavaDudeCleanUpOptionsInitializer.TAB_SIZE, text.getText());
+			}});
+
+		return composite;
+	}
+
+	@Override
+	public int getCleanUpCount() {
+		return 1;
+	}
+
+	@Override
+	public String getPreview() {
+		return "TBD\nTBD\nTBD\nTBD\nTBD\nTBD\nTBD\nTBD\nTBD\nTBD\n";
+	}
+
+	@Override
+	public int getSelectedCleanUpCount() {
+		return options.isEnabled(JavaDudeCleanUpOptionsInitializer.CONVERT_TABS_TO_SPACES) ? 1 : 0;
+	}
+
+	@Override
+	public void setOptions(CleanUpOptions options) {
+		this.options = options;
+	}
+}
---
+++ trunk/com.javadude.cleanup/src/com/javadude/cleanup/JavaDudeCleanUpFix.java
@@ -0,0 +1,79 @@
+package com.javadude.cleanup;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.refactoring.CompilationUnitChange;
+import org.eclipse.jdt.ui.cleanup.CleanUpOptions;
+import org.eclipse.jdt.ui.cleanup.ICleanUpFix;
+import org.eclipse.text.edits.ReplaceEdit;
+
+public class JavaDudeCleanUpFix implements ICleanUpFix {
+	private ICompilationUnit compilationUnit;
+	private final CleanUpOptions options;
+
+	public JavaDudeCleanUpFix(ICompilationUnit compilationUnit, CleanUpOptions options) {
+		this.compilationUnit = compilationUnit;
+		this.options = options;
+	}
+
+	@Override
+	public CompilationUnitChange createChange(IProgressMonitor progressMonitor)
+			throws CoreException {
+		if (options.isEnabled(JavaDudeCleanUpOptionsInitializer.CONVERT_TABS_TO_SPACES)) {
+			String source = compilationUnit.getSource();
+			ReplaceEdit edit = new ReplaceEdit(0, source.length(), fixTabs(source, Integer.parseInt(options.getValue(JavaDudeCleanUpOptionsInitializer.TAB_SIZE))));
+			CompilationUnitChange change = new CompilationUnitChange("Convert Tabs to Spaces", compilationUnit);
+			change.setEdit(edit);
+			return change;
+		}
+		return null;
+	}
+
+	// grrrr... I hate that I have to explicitly replace tabs with spaces to get anything other than 8-space tabs...
+	private String fixTabs(String string, int tabSize) {
+		String[] pads = new String[tabSize+1];
+		for (int i = 0; i <= tabSize; i++) {
+			pads[i] = "";
+			for (int j = 0; j < i; j++) {
+				pads[i] += ' ';
+			}
+		}
+
+		int len = string.length();
+		int column = 1; // position in line
+		StringBuilder b = new StringBuilder();
+		boolean inTag = false;
+		for (int n = 0; n < len; n++) {
+			char c = string.charAt(n);
+			if (inTag) {
+				if (c == '>') {
+					inTag = false;
+				}
+				b.append(c);
+				continue;
+			}
+			switch (c) {
+				case '<':
+					inTag = true;
+					b.append(c);
+					break;
+				case '\t':
+					int pad = tabSize - (column % tabSize);
+					b.append(pads[pad]);
+					column += pad;
+					break;
+				case '\n':
+				case '\r':
+					column = 0;
+					b.append(c);
+					break;
+				default:
+					column++;
+					b.append(c);
+					break;
+			}
+		}
+		return b.toString();
+	}
+}
