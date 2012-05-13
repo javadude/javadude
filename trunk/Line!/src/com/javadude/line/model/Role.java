5c23eacd5ca31592b77cbb122c65fbb5c05f143d
scott@javadude.com <scott@javadude.com@88cfd0be-b251-0410-a78a-4b8c7298577f>
2012-05-13 17:07:16 +0000

    added support for italics

--- trunk/Line!/src/com/javadude/line/model/Role.java
+++ trunk/Line!/src/com/javadude/line/model/Role.java
@@ -2,10 +2,13 @@ package com.javadude.line.model;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Role {
 	private String name;
+	private Set<Integer> pagesThisRoleAppears = new HashSet<Integer>();
 	private List<Line> lines = new ArrayList<Line>();
 
 	public Role(String name) {
@@ -16,8 +19,12 @@ public class Role {
 		return name;
 	}
 
+	public Set<Integer> getPagesThisRoleAppears() {
+		return pagesThisRoleAppears;
+	}
 	public void add(Line line) {
 		lines.add(line);
+		pagesThisRoleAppears.add(line.getPage());
 	}
 	public List<Line> getLines() {
 		return Collections.unmodifiableList(lines);
