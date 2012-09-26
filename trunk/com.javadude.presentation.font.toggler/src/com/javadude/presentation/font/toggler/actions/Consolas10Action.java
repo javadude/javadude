7817da33eddb87fabf7d32ba4c7d6a2499641e13
scott@javadude.com <scott@javadude.com@88cfd0be-b251-0410-a78a-4b8c7298577f>
2012-09-26 19:57:00 +0000

    Add Source Code Pro and make buttons smaller

---
+++ trunk/com.javadude.presentation.font.toggler/src/com/javadude/presentation/font/toggler/actions/Consolas10Action.java
@@ -0,0 +1,7 @@
+package com.javadude.presentation.font.toggler.actions;
+
+public class Consolas10Action extends TogglePresentationFontsAction {
+	public Consolas10Action() {
+		super(TogglePresentationFontsAction.CONSOLAS_10);
+	}
+}
---
+++ trunk/com.javadude.presentation.font.toggler/src/com/javadude/presentation/font/toggler/actions/SCP10Action.java
@@ -0,0 +1,7 @@
+package com.javadude.presentation.font.toggler.actions;
+
+public class SCP10Action extends TogglePresentationFontsAction {
+	public SCP10Action() {
+		super(TogglePresentationFontsAction.SCP_10);
+	}
+}
